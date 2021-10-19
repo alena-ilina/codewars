380a2cf74c6f1d27d52e3dd96ede5036f26e8c55
skachko.e <skachko.e@skbkontur.ru>
2021-10-19 09:49:47 +0300

    Добавила сетап, сломала тесты, поправила опечатки

--- tickets/task.js
+++ tickets/task.js
@@ -15,22 +15,22 @@ function getPriceTicket(currentPrice, reason1, reason2) {
 
   if (reason1 === "Длительная гроза" || reason2 === "Длительная гроза") {
     currentPrice = currentPrice * 1.2;
-    result = `Самолет промок. Цена билета увеличина на 20%`
+    result = `Самолет промок. Цена билета увеличена на 20%`
   }
 
   if (reason1 === "Лето, солнце" || reason2 === "Лето, солнце") {
     currentPrice = currentPrice * 1.4;
-    result = `${result ? `${result}. ` : ''}Все хотят в отпуск. Цена билета увеличина на 40%`
+    result = `${result ? `${result}. ` : ''}Все хотят в отпуск. Цена билета увеличена на 40%`
   }
 
   if (reason1 === "Рейс с питанием" || reason2 === "Рейс с питанием") {
     currentPrice = currentPrice * 1.05;
-    result = `${result ? `${result}. ` : ''}Еда стоит денег. Цена билета увеличина на 5%`
+    result = `${result ? `${result}. ` : ''}Еда стоит денег. Цена билета увеличена на 5%`
   }
 
   if (reason1 === "Самолет с широкими креслами" || reason2 === "Самолет с широкими креслами") {
     currentPrice = currentPrice * 1.07;
-    result = `${result ? `${result}. ` : ''}За комфорт надо платить. Цена билета увеличина на 7%`
+    result = `${result ? `${result}. ` : ''}За комфорт надо платить. Цена билета увеличена на 7%`
   }
 
   return `${result}. Итого цена билета:${currentPrice}`
