5dbe39fe9ea1f59be4e9e682446aef78b4a47c64
vladimir.parshuta <vladimir.parshuta@skbkontur.ru>
2023-05-26 15:55:24 +0700

    Little fixes

--- weatherTask/task.js
+++ weatherTask/task.js
@@ -1,7 +1,9 @@
 /* Код определяет, какая погода "ощущается"
 в зависимости от переданных параметров. Перепиши код так, 
-чтобы все тесты в соседнем файле прошли успешно
-и котик получил свой клубок. Удачи!
+чтобы все тесты в соседнем файле прошли успешно.
+Тогда котик получит свой клубок. Удачи!
+P.S. Не забывай нажимать ctrl+S, чтобы видеть результаты 
+своих правок.
 */
 
 let reason = {
@@ -23,55 +25,78 @@ function getFeelingTheeWeather(reason){
     }
 
     finalTemperature = reason["degrees"];
+
     if(reason["degrees"] <= 15) {
+        
         if(reason["wet"] <= 34)
         { finalTemperature =  finalTemperature + 1}
+        
         if(reason["wet"] <= 49 && reason["wet"] >= 35)
         { finalTemperature =  finalTemperature }
+        
         if(reason["wet"] <= 69 && reason["wet"] >= 50)
         { finalTemperature =  finalTеmperature - 1}
+        
         if(reason["wet"] <= 100 && reason["wet"] >= 70)
         { finalTemperature =  finalTemperature - 2}
 
+        
         if(reason["wind"] <= 8)
         { finalTemperature =  finalTemperature }
+        
         if(reason["wind"] <= 13 && reason["wind"] >= 9)
         { finalTemperature =  finalTemperature - 1}
+        
         if(reason["wind"] <= 20 && reason["wind"] >=14)
         { finalTemperature =  finalTemperature - 2}
+        
         if(reason["wind"] <= 100 && reason["wind"] >= 21)
         { finalTemperature =  finalTemperature - 3}
 
+        
         if(reason["cloudy"] <= 10 && reason["cloudy"] >=0)
         { finalTemperature =  finalTemperature + 2 }
+        
         if(reason["cloudy"] <= 40 && reason["cloudy"] >=11)
         { finalTemperature =  finalTemperature + 1 }
+        
         if(reason["cloudy"] <= 100 && reason["cloudy"] >=41)
         { finalTemperature = finalTemperature}
+        
         if(reason["cloudy"] === undefined)
         { return "Кажется ты забыл передать какой-то параметр"}
     }
     if( 16 <= reason["degrees"] ) {
+        
         if(reason["wet"] <= 49)
         { finalTemperature =  finalTemperature }
+        
         if(reason["wet"] <= 69 && reason["wet"] >= 50)
         { finalTemperature =  finalTemperature + 1 }
+        
         if(reason["wet"] <= 100 && reason["wet"] >= 70)
         { finalTemperature =  finalTemperature + 2}
 
+        
         if(reason["wind"] <= 8)
         { finalTemperature =  finalTemperature + 1 }
+        
         if(reason["wind"] <= 20 && reason["wind"] >= 9)
         {  finalTemperature =  finalTemperature - 1 }
+        
         if(reason["wind"] <= 100 && reason["wind"] >= 21)
         { finalTemperature = finalTemperature - 2}
 
+        
         if(reason["cloudy"] <= 10 && reason["cloudy"] >= 0)
         { finalTemperature += finalTemperature + 4}
+        
         if(reason["cloudy"] <= 40 && reason["cloudy"] >= 11)
         { finalTemperature =  finalTemperature + 3}
+        
         if(reason["cloudy"] <= 70 && reason["cloudy"] >= 41)
         { finalTemperature =  finalTemperature + 2}
+        
         if(reason["cloudy"] <= 100 && reason["cloudy"] >= 71)
         { finalTemperature =  finalTemperature }
     };
